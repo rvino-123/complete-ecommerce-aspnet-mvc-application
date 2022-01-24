323b913e0d3d59b1bb082e447f7695ad61b988e0
Roman Vinokurov <roman.vinokurov96@gmail.com>
2022-01-24 17:40:06 +0000

    Added last bits of improvement

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -37,8 +38,14 @@ namespace eTickets.Controllers
 
             if(!string.IsNullOrEmpty(searchString))
             {
-                var filteredResult = allMovies.Where(n => n.Name.Contains(searchString) || n.Description.Contains(searchString)).ToList();
-                return View("Index", filteredResult);
+                //var filteredResult = allMovies.Where(n => n.Name.ToLower().Contains(searchString.ToLower()) || 
+                //    n.Description.Contains(searchString.ToLower())).ToList();
+
+                var filteredResultNew = allMovies.Where(n => string.Equals(n.Name, searchString, 
+                    StringComparison.CurrentCultureIgnoreCase) || string.Equals(n.Description, searchString,
+                    StringComparison.CurrentCultureIgnoreCase)).ToList();
+
+                return View("Index", filteredResultNew);
             }
             return View("Index",allMovies);
         }
